80c77c77c59e63c1e3817c89f74a6306dc5769ac
Ajmal Hifzi <hifziajmal12@gmail.com>
2026-05-21 16:14:43 +0800

    Fix Express 5 wildcard options route

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 
